864f9b6e6de481ba98661e06f1e72970ad6fc75d
sahil <sahil91098singh@gmail.com>
2020-06-12 00:50:19 +0530

    Minor change.

--- app/src/main/java/com/example/jabwemate/model/Dog.java
+++ app/src/main/java/com/example/jabwemate/model/Dog.java
@@ -1,12 +1,14 @@
 package com.example.jabwemate.model;
 
+import java.util.ArrayList;
+
 public class Dog {
 
     public String dogname;
     public String dogage;
     public String doggender;
     public String dogbreed;
-    public String imageurl;  // Firebase storage download URL
+    public ArrayList<String> imageurl;  // Firebase storage download URL
     public String ownername;
     public String ownerphone;
 
