83e462d9d074b17a055c7371bfd1e2cda52375cb
sahil <sahil91098singh@gmail.com>
2020-06-12 12:45:20 +0530

    User info activity added.

---
+++ app/src/main/java/com/example/jabwemate/ChangePasswordActivity.java
@@ -0,0 +1,15 @@
+package com.example.jabwemate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_change_password);
+
+   }
+}
